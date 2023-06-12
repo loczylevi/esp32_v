803e8a3b6e98f339a0afca39a77a5278c96b1056
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2023-06-12 19:00:36 +0200

    Create morze_esp.py

---
+++ morze_esp32/morze_esp.py
@@ -0,0 +1,32 @@
+import machine
+import time
+import random
+sw = machine.Pin(0,machine.Pin.IN)
+led = machine.Pin(2, machine.Pin.OUT)
+hangszoro = machine.Pin(26, machine.Pin.OUT)
+pwm = machine.PWM(hangszoro)
+pwm.freq(500)
+pwm.duty(0)
+
+def handle_interrupt(pin):
+    print("\n______________\tMorze kód elindult\t______________\n")
+    leforditando = "...---..."
+    for betu in leforditando:
+        if betu == ".":
+            pwm.duty(100)
+            time.sleep(0.10)
+            pwm.duty(0)
+        elif betu == "-":
+            pwm.duty(100)
+            time.sleep(0.25)
+            pwm.duty(0)
+        time.sleep(0.10)
+
+
+sw.irq(trigger=machine.Pin.IRQ_FALLING, handler=handle_interrupt)  #ha fut egy loop és ez aktiválodik akkor leállít mindentés ezt futtatja szoval abba hagyja a randomdolgok kiválasztását a listából és elkezdi prioritássalkiirni a '!' jeleket
+
+lista = ["morze fos","NE PINGELJÉ MÁ'","bojler eladó ugyanitt","BUKSI HOL VAGY?","kiesett az aranyhal az akváriumból","mind megbukunk let's gooo","carpe diem","deus ex machine","divide et impara","cuius regio eius religio","in medium res","panem et cirsensen","Si vis pacem, para bellum.","Alea iacta est.","Ave Caesar, morituri te salutant!","Cogito, ergo sum","Dum spiro, spero","Nomen est omen","Repetitio est mater studiorum.","Veni, Vidi, Vici!"]
+while True:
+    print("\nirjon be egy istenverte morzét", random.choice(lista))
+    time.sleep(0.1)
+    
